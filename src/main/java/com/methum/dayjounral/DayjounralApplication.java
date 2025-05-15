e87096e6d071ad722551b4bd4c77defe4f3b0b2f
The-OG-Methum <moneysenseed@gmail.com>
2025-05-15 15:00:25 +0530

    set up the directory

---
+++ src/main/java/com/methum/dayjounral/DayjounralApplication.java
@@ -0,0 +1,13 @@
+package com.methum.dayjounral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayjounralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayjounralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/methum/dayjounral/DayjounralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.methum.dayjounral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayjounralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
